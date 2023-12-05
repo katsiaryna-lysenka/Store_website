8201e5d750980cadcdc0d1d22b115c3459429a15
Katsiaryna Lysenka <katya.lysenko.1999@gmail.com>
2023-12-05 09:57:45 +0200

    remove space

--- my_shop/cart/views.py
+++ my_shop/cart/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
@@ -7,15 +8,20 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product,
-                 quantity=cd['quantity'],
-                 update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    registration_link = "http://127.0.0.1:8000/login/"
+    registration_message = f"Пожалуйста, пройдите <a href='{registration_link}'>регистрацию</a>."
+    if not request.user.is_authenticated:
+        return HttpResponse(registration_message)
+    else:
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddProductForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            cart.add(product=product,
+                     quantity=cd['quantity'],
+                     update_quantity=cd['update'])
+        return redirect('cart:cart_detail')
 
 
 def cart_remove(request, product_id):
